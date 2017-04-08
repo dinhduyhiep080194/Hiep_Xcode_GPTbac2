7d4acf76ee042e4ff387c0c4de358bb08cef9689
cntt17 <cntt17@cntt17s-iMac.local>
2017-04-08 11:27:21 +0700

    Initial Commit

--- GiaiPTBac2/ViewController.swift
+++ GiaiPTBac2/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txta: UITextField!
+    @IBOutlet var txtb: UITextField!
+    @IBOutlet var txtc: UITextField!
+    
+
+    @IBOutlet var lblResult2: UILabel!
+    @IBOutlet var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblResult.isHidden = true
+        lblResult2.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnGiai(_ sender: Any) {
+        lblResult2.text = ""
+        let a:Double = Double(txta.text!)!
+        let b:Double = Double(txtb.text!)!
+        let c:Double = Double(txtc.text!)!
+        lblResult.isHidden = false
+        if(a==0){
+            if(b==0){
+                if(c==0){
+                    lblResult.text = "PT co nghiem bat ki"
+                }
+                else{
+                    lblResult.text = "PT vo nghiem"
+                }
+            }
+            else{
+                lblResult.text = "PT co nghiem x = \(-c/b)"
+            }
+        }
+        else{
+            let delta = ((b*b)-(4*a*c))
+            if(delta<0){
+            lblResult.text = "PT vo nghiem"
+            }
+            else if(delta==0){
+                let x = (-b)/(2*a)
+                lblResult.text = "PT co nghiem x = \(x)"
+            }
+                else{
+                    lblResult2.isHidden = false
+                    let x1 = (-b + sqrt(delta))/(2*a)
+                    let x2 = (-b - sqrt(delta))/(2*a)
+                    lblResult.text = "PT co nghiem x1 = \(x1)"
+                    lblResult2.text = "PT co nghiem x2 = \(x2)"
+                }
+            }
+    
+    }
 }
 
